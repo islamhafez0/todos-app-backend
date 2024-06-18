2ade07d81811cd97afcf335af98b457f50ec52b4
islamhafez0 <islamhafez872@gmail.com>
2024-06-18 14:58:19 +0300

    update cors configs

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const router = require("./routes/todos");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 const PORT = process.env.PORT || 8000;
 const dataBaseUrl = process.env.DATABASE_URL;
