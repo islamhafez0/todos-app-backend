4e14ce32a110322908232357fcb466f71cd0c8e4
islamhafez0 <islamhafez872@gmail.com>
2024-06-23 18:11:19 +0300

    add cors configuration

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const router = require("./routes/todos");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:8000", "https://todos-app-red.vercel.app/"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 const PORT = process.env.PORT || 8000;
 const dataBaseUrl = process.env.DATABASE_URL;
